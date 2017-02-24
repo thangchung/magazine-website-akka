58201c8a57f94dbd9df4033eafc3b5a4ee55d32f
thangchung <thangchung@ymail.com>
2017-02-24 15:08:57 +0700

    release resource when application goes down

--- src/Cik.Magazine.Web/Startup.cs
+++ src/Cik.Magazine.Web/Startup.cs
@@ -18,6 +18,10 @@ namespace Cik.Magazine.Web
 {
     public class Startup
     {
+        private ActorSystem _systemActor;
+        private IActorRef _categoryQueryActor;
+        private IActorRef _categoryCommanderActor;
+
         public Startup(IHostingEnvironment env)
         {
             var builder = new ConfigurationBuilder()
@@ -74,16 +78,18 @@ namespace Cik.Magazine.Web
                 });
 
             // build up the actors
-            var systemActor = ActorSystem.Create("magazine-system");
-            systemActor.ActorOf(Props.Empty.WithRouter(FromConfig.Instance), "category-query-group");
-            systemActor.ActorOf(Props.Empty.WithRouter(FromConfig.Instance), "category-commander-group");
+            _systemActor = ActorSystem.Create("magazine-system");
+            _categoryQueryActor = _systemActor.ActorOf(Props.Empty.WithRouter(FromConfig.Instance), "category-query-group");
+            _categoryCommanderActor = _systemActor.ActorOf(Props.Empty.WithRouter(FromConfig.Instance), "category-commander-group");
 
-            services.AddSingleton<IActorRefFactory>(serviceProvider => systemActor);
+            services.AddSingleton<IActorRefFactory>(serviceProvider => _systemActor);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        public void Configure(IApplicationBuilder app, IApplicationLifetime applicationLifetime, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
+            applicationLifetime.ApplicationStopping.Register(OnShutdown);
+
             loggerFactory.AddConsole(Configuration.GetSection("Logging"));
             loggerFactory.AddDebug();
 
@@ -92,7 +98,7 @@ namespace Cik.Magazine.Web
             {
                 Authority = "http://localhost:9999/",
                 RequireHttpsMetadata = false // don't use this for production env
-            }); */  
+            }); */
 
             app.UseMvc();
 
@@ -102,6 +108,17 @@ namespace Cik.Magazine.Web
                 c.SwaggerEndpoint("/swagger/v1/swagger.json", "Magazine Website API V1");
                 // c.ConfigureOAuth2("swagger", "secret".Sha256(), "swagger", "swagger");
             });
+        }
+
+        /// <summary>
+        /// https://stackoverflow.com/questions/35257287/kestrel-shutdown-function-in-startup-cs-in-asp-net-core
+        /// https://shazwazza.com/post/aspnet-core-application-shutdown-events/
+        /// </summary>
+        private void OnShutdown()
+        {
+            _categoryQueryActor.Tell(PoisonPill.Instance);
+            _categoryCommanderActor.Tell(PoisonPill.Instance);
+            _systemActor.Terminate().Wait(5000);
         }
     }
 }
